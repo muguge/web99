12560543998e156521b3b30dd77683eddb04fbe4
lili <3487054226@qq.com>
2021-04-12 15:13:25 +0800

    by man

---
+++ 2.js
@@ -0,0 +1,4 @@
+let objec1 = new Object(); //66//为空
+console.log(objec1.__proto__); //[Number:0]//{}
+console.log(objec1.__proto__.__proto__); //{}//null
+alert("我的第一个js")
---
+++ 数据结构/1.c
@@ -0,0 +1,6 @@
+#define MaxSize 50  //定义线性表的最大长度
+typedef int Elemtype//假定表中元素类型是int
+typedf struct{
+    ElemType data[MaxSize];//顺序表的元素（数组）
+    int length;//顺序表的当前长度
+}SqList;//顺序表的类型定义
